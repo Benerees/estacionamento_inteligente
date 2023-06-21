340eb7cc832c09bd52da62d3ab927264e978672e
Daniel Ferreira Martins <danielfemartins@gmail.com>
2023-06-20 23:57:35 -0300

    feat:

---
+++ src/training_YOLO.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+# Use the model
+results = model.train(data="config.yaml", epochs=1)  # train the model
+metrics = model.val()  # evaluate model performance on the validation set
