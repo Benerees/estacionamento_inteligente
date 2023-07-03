d2736ec4969fb71d93e010577bebd31f42c832d1
Daniel Ferreira Martins <danielfemartins@gmail.com>
2023-07-03 17:46:41 -0300

    feat: imagens

--- src/training_YOLO.py
+++ src/training_YOLO.py
@@ -1,12 +1,9 @@
 from ultralytics import YOLO
 
-
-
-
 model = YOLO("yolov8n.yaml")  
 model = YOLO("yolov8n.pt")  
 
 
-results = model.train(data="config.yaml", epochs=100, patience = 45, conf = 0.4)  
+results = model.train(data="config.yaml", epochs=100, patience = 50, conf = 0.4)  
 metrics = model.val() 
 
